d729fd0ee4e427d81422e824606c23c33563d7ce
Voltaicblood <ajayye@hotmail.com>
2016-07-26 09:38:01 -0400

    Beekeeper Project Feature Complete

--- HW/Projects/Beekeeper/BeekeeperEarnings.java
+++ HW/Projects/Beekeeper/BeekeeperEarnings.java
@@ -9,50 +9,52 @@ CS101-03
 */
 
 import javax.swing.*;
+import java.text.*;
 
 public class BeekeeperEarnings
 {
 
 /*
-                 Algorithm
+                   Algorithm
 main(args)
-  COST_OF_CASE <-- 4536
-  COST_OF_COMB <-- 249
-  COST_OF_RAW <-- 99
-  COST_OF_RENT <-- 53499
-  
-  JARS_IN_CASE <-- 24
-  
-  jarSold <-- input from dialog asking for amount of jars sold
-  jarSoldNum <-- convert jarSold to integer
-  jarSoldNum <-- round to nearest whole number
-  caseSold <-- jarSoldNum / JARS_IN_CASE
-  
-  combSold <-- input from dialog asking for how many lbs of honey combs sold
-  rawSold <-- input from dialog asking for how many lbs of raw honey sold
-  rentSold <-- input from dialog asking for how many hives were rented
-                 out for a week
-                 
-  caseIncome <-- caseSold * COST_OF_CASE
-  combIncome <-- convert combSold to integer, then multiply it by COST_OF_COMB
-  rawIncome <-- convert rawSold to integer, then multiply it by COST_OF_RAW
-  rentIncome <-- convert rentSold to integer, then multiply it by COST_OF_RENT
-  
-  totalIncome <-- caseIncome + combIncome + rawIncome + rentIncome
-  
-  dollarPattern <-- "$#,##0.00" decimal format
-  
-  display message caseIncome using dollarPattern, labeled
-  display message combIncome using dollarPattern, labeled
-  display message rawIncome using dollarPattern, labeled
-  display message rentIncome using dollarPattern, labeled
-  display message totalIncome using dollarPattern, labeled
-                 
-              
+    COST_OF_CASE <-- 4536
+    COST_OF_COMB <-- 249
+    COST_OF_RAW <-- 99
+    COST_OF_RENT <-- 53499
+    
+    JARS_IN_CASE <-- 24
+    
+    jarSold <-- input from dialog asking for amount of jars sold
+    jarSoldNum <-- convert jarSold to double
+    caseSold <-- jarSoldNum / JARS_IN_CASE
+    caseSold <-- round to nearest whole number
+    
+    combSold <-- input from dialog asking for how many lbs of honey combs sold
+    rawSold <-- input from dialog asking for how many lbs of raw honey sold
+    rentSold <-- input from dialog asking for how many hives were rented
+                   out for a week
+                   
+    caseIncome <-- caseSold * COST_OF_CASE
+    combIncome <-- convert combSold to double, then multiply it by COST_OF_COMB
+    rawIncome <-- convert rawSold to double, then multiply it by COST_OF_RAW
+    rentIncome <-- convert rentSold to double, then multiply it by COST_OF_RENT
+    
+    totalIncome <-- caseIncome + combIncome + rawIncome + rentIncome
+    
+    dollarPattern <-- "$#,##0.00"
+    dollar <-- decimal format using dollarPattern
+    
+    display message caseIncome using dollarPattern, labeled
+    display message combIncome using dollarPattern, labeled
+    display message rawIncome using dollarPattern, labeled
+    display message rentIncome using dollarPattern, labeled
+    display message totalIncome using dollarPattern, labeled
+                   
+                
 */
 
 /*
-                  Data Table
+                    Data Table
 Variable or Constant          Purpose
 ____________________          _________________________________________
 args                          parameter of main
@@ -72,13 +74,45 @@ combIncome                    Gross income from selling honeycomb
 rawIncome                     Gross income from selling raw honey
 rentIncome                    Gross income from renting out hives
 totalIncome                   Total gross income from all sources
-dollarPattern                 Formating for dollars and cents.
-                  
+dollarPattern                 Patten to format for dollars and cents.
+dollar                        Decimal format using dollarPattern.
+                    
 */
 
-  public static void main(String [] args)
-  {
-  
-  }//main method end
+   public static void main(String [] args)
+   {
+      final double COST_OF_CASE = 45.36;
+      final double COST_OF_COMB = 2.49;
+      final double COST_OF_RAW = 0.99;
+      final double COST_OF_RENT = 534.99;
+      
+      final double JARS_IN_CASE = 24;
+      
+      String jarSold = JOptionPane.showInputDialog(null, "Input number of honey jars sold:");
+      double jarSoldNum = Double.parseDouble(jarSold);
+      double caseSold = jarSoldNum / JARS_IN_CASE;
+      caseSold = Math.rint(caseSold);
+      
+      String combSold = JOptionPane.showInputDialog(null, "Input in pounds the amount of honey combs sold:");
+      String rawSold = JOptionPane.showInputDialog(null, "Input in pounds the amount of raw honey sold:");
+      String rentSold = JOptionPane.showInputDialog(null, "Input how many hives were rented out for a week:");
+                     
+      double caseIncome = caseSold * COST_OF_CASE;
+      double combIncome = Double.parseDouble(combSold) * COST_OF_COMB;
+      double rawIncome = Double.parseDouble(rawSold) * COST_OF_RAW;
+      double rentIncome = Double.parseDouble(rentSold) * COST_OF_RENT;
+      
+      double totalIncome = caseIncome + combIncome + rawIncome + rentIncome;
+      
+      String dollarPattern = "$#,##0.00";
+      DecimalFormat dollar = new DecimalFormat(dollarPattern);
+      
+      JOptionPane.showMessageDialog(null, "Income from selling cases of honey jars is: " + dollar.format(caseIncome));
+      JOptionPane.showMessageDialog(null, "Income from selling honey combs is: " + dollar.format(combIncome));
+      JOptionPane.showMessageDialog(null, "Income from selling raw honey is: " + dollar.format(rawIncome));
+      JOptionPane.showMessageDialog(null, "Income from renting out hives is: " + dollar.format(rentIncome));
+      JOptionPane.showMessageDialog(null, "Total income from all sources is: " + dollar.format(totalIncome));
+     
+   }//main method end
 
 }//BeekeeperEarnings class end
